ccf53219df60020a8d0c649e7425df205870b68c
Ivan Kharitonov <ipkharitonov@gmail.com>
2021-03-25 23:41:08 +0300

    feat: adds nox config

---
+++ noxfile.py
@@ -0,0 +1,19 @@
+# noxfile.py
+
+import nox
+
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+@nox.session
+def tests(session):
+    session.run("poetry", "install", external=True)
+    session.install("pytest")
+    session.run("pytest", "-v", "tests")
