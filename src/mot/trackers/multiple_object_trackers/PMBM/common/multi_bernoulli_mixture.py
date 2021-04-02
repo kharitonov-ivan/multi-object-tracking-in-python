1ea7069a13f37a256696d83efbb52ad16c8c610a
Ivan Kharitonov <ipkharitonov@gmail.com>
2021-04-02 13:01:23 +0300

    refactor: move methods to other class

--- src/mot/trackers/multiple_object_trackers/PMBM/common/multi_bernoulli_mixture.py
+++ src/mot/trackers/multiple_object_trackers/PMBM/common/multi_bernoulli_mixture.py
@@ -16,7 +16,7 @@ class MultiBernouilliMixture:
 
     def __init__(self):
         self.tracks = {}
-        self.global_hypotheses = []
+        self.global_hypotheses: List[GlobalHypothesis] = []
 
     def __repr__(self) -> str:
         return (
@@ -29,21 +29,21 @@ class MultiBernouilliMixture:
         )
 
     def add_track(self, track: Track):
-        assert track.track_id in self.tracks.keys()
+        assert not (track.track_id in self.tracks.keys())
         self.tracks[track.track_id] = track
 
     def estimator(self):
         """Simply return objects set based on most probable global hypo."""
         if self.global_hypotheses:
             most_probable_global_hypo = max(
-                self.global_hypotheses, key=lambda x: x.weight
+                self.global_hypotheses, key=lambda x: x.log_weight
             )
         else:
             logging.info("Pool of global hypotheses is empty!")
             return None
 
         object_list = []  # list of {'object_id':'object_state'}
-        for (track_id, sth_id) in most_probable_global_hypo.hypothesis:
+        for (track_id, sth_id) in most_probable_global_hypo.associations:
             object_state = (
                 self.tracks[track_id].single_target_hypotheses[sth_id].bernoulli.state.x
             )
@@ -51,14 +51,17 @@ class MultiBernouilliMixture:
 
         return object_list
 
-    def predict(self, motion_model, survival_probability, density_handler, dt) -> None:
+    def predict(
+        self,
+        motion_model: MotionModel,
+        survival_probability: float,
+        density_handler,
+        dt: float,
+    ) -> None:
         # MBM predict
-        for track_id, track in self.tracks.items():
-            for sth_id, sth in track.single_target_hypotheses.items():
-                self.tracks[track_id].single_target_hypotheses[
-                    sth_id
-                ].bernoulli = Bernoulli.predict(
-                    track.single_target_hypotheses[sth_id].bernoulli,
+        for track in self.tracks.values():
+            for sth in track.single_target_hypotheses.values():
+                sth.bernoulli.predict(
                     motion_model,
                     survival_probability,
                     density_handler,
@@ -87,7 +90,14 @@ class MultiBernouilliMixture:
 
         return (gating_matrix, used_measurement_detected_indices)
 
-    def update(self, detection_probability: float, z: np.ndarray, gating_matrix, meas_model) -> None:
+    def update(
+        self,
+        detection_probability: float,
+        measurements: np.ndarray,
+        gating_matrix,
+        meas_model,
+        density,
+    ) -> None:
         """For each Bernoulli state density,
         create a misdetection hypothesis (Bernoulli component), and
         m object detection hypothesis (Bernoulli component),
@@ -96,56 +106,24 @@ class MultiBernouilliMixture:
         used_meas_indices : (hyp_tree_idx x bern_idx x meas_idx)
             [description]
         """
-
-        for track_id, track in self.tracks.items():
-            for sth_id, sth in track.single_target_hypotheses.items():
-                sth.children = {}
-                # Create missdetection hypothesis
-                (
-                    bernoulli_undetected,
-                    log_likelihood_undetected,
-                ) = sth.bernoulli.undetected_update(detection_probability)
-
-                missdetection_hypothesis = SingleTargetHypothesis(
-                    bernoulli_undetected,
-                    log_likelihood_undetected,
-                    associated_measurement_idx=-1,
-                    cost=None,
+        for track in self.tracks.values():
+            for sth in track.single_target_hypotheses.values():
+                sth.missdetection_hypothesis = sth.create_missdetection_hypothesis(
+                    detection_probability, next(track.sth_id_generator)
                 )
-                logging.debug(
-                    f"track id ={track_id}, sth_id = {sth_id}, {self.tracks[track_id].single_target_hypotheses[sth_id].bernoulli}"
-                )
-                logging.debug(f"missdetection likelihood {log_likelihood_undetected}")
-                sth.children.update({-1: missdetection_hypothesis})
 
-                log_likelihoods_detected = Bernoulli.detected_update_likelihood(
-                    sth.bernoulli,
-                    z,
-                    meas_model,
-                    detection_probability,
-                )
-                # Create association hypothesis for measurements which in the gate
-                for meas_idx, meas in enumerate(z):
-                    bernoulli_detected = Bernoulli.detected_update_state(
-                        sth.bernoulli,
-                        z[meas_idx],
+                sth.detection_hypotheses = {
+                    meas_idx: sth.create_detection_hypothesis(
+                        measurements[meas_idx],
+                        detection_probability,
                         meas_model,
+                        density,
+                        next(track.sth_id_generator),
                     )
-                    logging.debug(
-                        f"detection z = {meas} likelihood {log_likelihoods_detected[meas_idx]}"
-                    )
-                    detection_hypothesis = SingleTargetHypothesis(
-                        bernoulli_detected,
-                        log_likelihoods_detected[meas_idx],
-                        meas_idx,
-                        cost=-(
-                            log_likelihoods_detected[meas_idx]
-                            - log_likelihood_undetected
-                        ),
-                    )
-                    sth.children.update({meas_idx: detection_hypothesis})
+                    for meas_idx in range(len(measurements))
+                }
 
-    def prune_global_hypotheses(self, threshold) -> None:
+    def prune_global_hypotheses(self, log_threshold: float) -> None:
         """Removes Bernoulli components with small probability of existence and reindex the hypothesis table.
         If a track contains no single object hypothesis after pruning, this track is removed
 
@@ -161,7 +139,7 @@ class MultiBernouilliMixture:
     def cap_global_hypothesis(self, max_number_of_global_hypothesis: int = 50):
         if len(self.global_hypotheses) > max_number_of_global_hypothesis:
             top_global_hypotheses = sorted(
-                self.global_hypotheses, key=lambda x: x.weight
+                self.global_hypotheses, key=lambda x: x.log_weight
             )
             self.global_hypotheses = top_global_hypotheses[
                 :max_number_of_global_hypothesis
@@ -198,11 +176,11 @@ class MultiBernouilliMixture:
 
     def normalize_global_hypotheses_weights(self) -> None:
         global_hypo_log_w_unnorm = [
-            global_hypothesis.weight for global_hypothesis in self.global_hypotheses
+            global_hypothesis.log_weight for global_hypothesis in self.global_hypotheses
         ]
         global_hypo_log_w_norm, _ = normalize_log_weights(global_hypo_log_w_unnorm)
 
         for global_hypo, normalized_log_weight in zip(
             self.global_hypotheses, global_hypo_log_w_norm
         ):
-            global_hypo.weight = normalized_log_weight
+            global_hypo.log_weight = normalized_log_weight
