847cfec1bf6ab9926a6fc4afe0075af4d512daa4
Ivan Kharitonov <ipkharitonov@gmail.com>
2021-04-15 14:45:09 +0300

    fix: replace hardcoded attr with property

--- src/mot/configs/ground_truth_config.py
+++ src/mot/configs/ground_truth_config.py
@@ -6,7 +6,6 @@ from .object_config import Object
 class GroundTruthConfig:
     def __init__(
         self,
-        n_births: int,
         object_configs: List[Object],
         total_time: int,
         *args,
@@ -24,14 +23,16 @@ class GroundTruthConfig:
             total tracking time
         """
 
-        assert isinstance(n_births, int), "Argument of wrong type!"
         assert isinstance(total_time, int), "Argument of wrong type!"
         assert all(
             isinstance(x, Object) for x in object_configs
         ), "Argument of wrong type!"
-        self.n_births = n_births
         self.object_configs = object_configs
         self.total_time = total_time
+
+    @property
+    def n_births(self):
+        return len(self.object_configs)
 
     def __repr__(self) -> str:
         return self.__class__.__name__ + (
