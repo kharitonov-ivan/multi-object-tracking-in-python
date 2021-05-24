9c58d37a25a499c212600f3735401ada584dd53a
Ivan Kharitonov <ipkharitonov@gmail.com>
2021-05-24 15:35:47 +0300

    chore: beatify and cleanup

--- src/mot/measurement_models/__init__.py
+++ src/mot/measurement_models/__init__.py
@@ -1,4 +1,6 @@
-from .base_measurement_model import MeasurementModel
-from .CT_measurement_model import CoordinateTurnMeasurementModel
-from .CV_measurement_model import ConstantVelocityMeasurementModel
-from .RangeBearing_model import RangeBearingMeasurementModel
+# flake8: noqa
+
+from mot.measurement_models.base_measurement_model import MeasurementModel
+from mot.measurement_models.CT_measurement_model import CoordinateTurnMeasurementModel
+from mot.measurement_models.CV_measurement_model import ConstantVelocityMeasurementModel
+from mot.measurement_models.RangeBearing_model import RangeBearingMeasurementModel
