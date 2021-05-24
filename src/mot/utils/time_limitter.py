cd7bee3e49f699b9763f22aa3dc7c516f8f84931
Ivan Kharitonov <ipkharitonov@gmail.com>
2021-05-24 13:05:32 +0300

    feat: adds time limitter which using as context manager

---
+++ src/mot/utils/time_limitter.py
@@ -0,0 +1,19 @@
+import signal
+from contextlib import contextmanager
+
+
+class TimeoutException(Exception):
+    pass
+
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
