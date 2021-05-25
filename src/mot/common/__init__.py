358092dfdba3df53d738c61160ee7b8177e8881b
Ivan Kharitonov <ipkharitonov@gmail.com>
2021-05-25 18:13:08 +0300

    fix: bug in import

--- src/mot/common/__init__.py
+++ src/mot/common/__init__.py
@@ -2,5 +2,5 @@
 
 from mot.common.gaussian_density import GaussianDensity
 from mot.common.hypothesis_reduction import HypothesisReduction
-from mot.normalize_log_weights import normalize_log_weights
-from mot.state import Gaussian, GaussianMixture, WeightedGaussian
+from mot.common.normalize_log_weights import normalize_log_weights
+from mot.common.state import Gaussian, GaussianMixture, WeightedGaussian
