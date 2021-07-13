3e0bfd151542af0d5db33aa3ff5facdac26a7aca
Ivan Kharitonov <ipkharitonov@gmail.com>
2021-07-13 22:34:06 +0300

    feat: adds data driven birth model

--- src/mot/trackers/multiple_object_trackers/PMBM/common/birth_model.py
+++ src/mot/trackers/multiple_object_trackers/PMBM/common/birth_model.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 
-from mot.common import GaussianMixture
+import numpy as np
+
+from mot.common import Gaussian, GaussianMixture, WeightedGaussian
 
 
 class BirthModel:
@@ -36,5 +38,19 @@ class MeasurementDrivenBirthModel(BirthModel):
     def __init__(self):
         super(MeasurementDrivenBirthModel, self).__init__()
 
-    def get_born_objects_intensity(self, measurements) -> GaussianMixture:
-        raise NotImplementedError
+    def get_born_objects_intensity(self, params) -> GaussianMixture:
+        measurements = params["measurements"]
+        num_of_born_components_per_measurement = 10
+
+        generated_intensity = []
+
+        random_width = 10
+        for measurement in measurements:
+            delta_x = np.random.uniform(-random_width, random_width, num_of_born_components_per_measurement)
+            delta_y = np.random.uniform(-random_width, random_width, num_of_born_components_per_measurement)
+
+            for d_x, d_y in zip(delta_x, delta_y):
+                state = np.array([measurement.measurement[0] + d_x, measurement.measurement[1] + d_y, 0.0, 0.0])
+                sample_gaussian = Gaussian(x=state, P=100 * np.eye(4))
+                generated_intensity.append(WeightedGaussian(log_weight=np.log(0.03), gaussian=sample_gaussian))
+        return GaussianMixture(generated_intensity)
