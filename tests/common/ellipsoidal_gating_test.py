c65a45c1f75df5966a2110284298e512de645849
Ivan Kharitonov <ipkharitonov@gmail.com>
2023-07-15 14:29:23 +0300

    update gating test

--- tests/common/ellipsoidal_gating_test.py
+++ tests/common/ellipsoidal_gating_test.py
@@ -1,57 +1,37 @@
 import numpy as np
 from scipy.stats import chi2
-
 from mot.common.gaussian_density import GaussianDensity
-from mot.common.state import Gaussian
-from mot.configs import GroundTruthConfig, Object, SensorModelConfig
+
 from mot.measurement_models import ConstantVelocityMeasurementModel
-from mot.motion_models import ConstantVelocityMotionModel
-from mot.simulator.measurement_data_generator import MeasurementData
-from mot.simulator.object_data_generator import ObjectData
 
 
-TOL = 1e-4
+def test_ellipsoidal_gating_known_values():
+    # Test setup
+    means = np.array([[10.0, 20.0, 30.0, 40.0]])  # (1, 4)
+    covs = np.array([[[2.0, 0.5, 0.1, 0.2], [0.5, 3.0, 0.1, 0.2], [0.1, 0.1, 2.0, 0.3], [0.2, 0.2, 0.3, 4.0]]])  # (1, 4, 4)
+    measurements = np.array([[11.0, 22.0], [12.0, 24.0]])  # ( 2, 2)
+    measurement_model = ConstantVelocityMeasurementModel(sigma_r=0.1)
+    confidence_level = 0.99
+
+    # Perform gating
+    meas_mask, mah_dists = GaussianDensity.ellipsoidal_gating(means, covs, measurements, measurement_model, confidence_level)
+
+    # Given the known inputs, check that the outputs have the expected values
+    assert meas_mask.all() == True, "At least one measurement should be inside the gate"
+    assert np.all(mah_dists < chi2.ppf(confidence_level, df=measurement_model.d)), "All measurements should be outside the gate"
 
 
-def test_ellipsoidal_gating():
-    n_births = 1
-    total_time = 10
-    objects = [
-        Object(
-            initial=Gaussian(x=np.array([0.0, 0.0, 5.0, 5.0]), P=np.eye(4)),
-            t_birth=0,
-            t_death=10,
-        )
-    ]
+def test_ellipsoidal_gating_known_values_outside_gate():
+    # Test setup
+    means = np.array([[10.0, 20.0, 30.0, 40.0]])  # (1, 4)
+    covs = np.array([[[2.0, 0.5, 0.1, 0.2], [0.5, 3.0, 0.1, 0.2], [0.1, 0.1, 2.0, 0.3], [0.2, 0.2, 0.3, 4.0]]])  # (1, 4, 4)
+    measurements = np.array([[100.0, 200.0], [200.0, 400.0]])  # (2, 2) - Measurements are far from the means
+    measurement_model = ConstantVelocityMeasurementModel(sigma_r=0.1)
+    confidence_level = 0.99
 
-    grount_truth_config = GroundTruthConfig(n_births=n_births, object_configs=objects, total_time=total_time)
-    test_dt = 1.0
-    test_sigma_q = 2.0
-    motion_model = ConstantVelocityMotionModel(dt=test_dt, sigma_q=test_sigma_q)
+    # Perform gating
+    meas_mask, mah_dists = GaussianDensity.ellipsoidal_gating(means, covs, measurements, measurement_model, confidence_level)
 
-    test_P_D = 1.0
-    test_lambda_c = 60.0
-    test_range_c = np.array([[-200.0, 200.0], [-200, 200]])
-    sensor_model = SensorModelConfig(P_D=test_P_D, lambda_c=test_lambda_c, range_c=test_range_c)
-
-    test_sigma_r = 10.0
-    meas_model = ConstantVelocityMeasurementModel(sigma_r=test_sigma_r)
-
-    object_data = ObjectData(grount_truth_config, motion_model, if_noisy=False)
-
-    meas_data = MeasurementData(object_data=object_data, sensor_model=sensor_model, meas_model=meas_model)
-
-    gating_size = chi2.ppf(0.99, df=meas_model.d)
-
-    states = [None for i in range(grount_truth_config.total_time)]
-    for timestep in range(0, grount_truth_config.total_time):
-        states[timestep] = Gaussian(x=np.array(object_data[timestep].x), P=np.eye(motion_model.d))
-
-        if timestep == 0:
-            continue
-        [z_ingate, meas_in_gate] = GaussianDensity.ellipsoidal_gating(
-            state_prev=states[timestep - 1],
-            z=np.array(meas_data[timestep]),
-            measurement_model=meas_model,
-            gating_size=gating_size,
-        )
+    # Given the known inputs, check that the outputs have the expected values
+    assert meas_mask.all() == False, "No measurement should be inside the gate"
+    assert np.all(mah_dists > chi2.ppf(confidence_level, df=measurement_model.d)), "All measurements should be outside the gate"
